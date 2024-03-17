892f69a5e96f6c041cb3d15b4ada6cd56ff4da17
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-03-18 00:38:15 +0400

    Interim result of Lesson 17 (transformation of auth and app reducers)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux'
-import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
+import {thunk as thunkMiddleware, ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
 import {configureStore, UnknownAction} from "@reduxjs/toolkit";
@@ -15,7 +15,9 @@ const rootReducer = combineReducers({
 
 // ❗старая запись, с новыми версиями не работает
 //  const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-export const store = configureStore({reducer: rootReducer},)
+export const store = configureStore({reducer: rootReducer,
+middleware: (getDefaultMiddleware)=>
+	getDefaultMiddleware().prepend(thunkMiddleware)},)
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
